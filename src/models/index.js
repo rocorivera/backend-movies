e1117295c7b9d19cfc079877eae9fc6eb37c6ee3
rober contreras <trebor.contrerasrivera@gmail.com>
2023-11-05 15:40:38 -0500

    mibackend

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+const Actor = require("./Actor");
+const Director = require("./Director");
+const Genre = require("./Genre");
+const Movie = require("./Movie");
+
+
+Movie.belongsToMany(Director,{through:'MovieDirectors'})
+Director.belongsToMany(Movie,{through:'MovieDirectors'})
+
+
+Movie.belongsToMany(Actor,{through:'MovieActors'})
+Actor.belongsToMany(Movie,{through:'MovieActors'})
+
+
+Movie.belongsToMany(Genre,{through:'MovieGenres'})
+Genre.belongsToMany(Movie,{through:'MovieGenres'})
